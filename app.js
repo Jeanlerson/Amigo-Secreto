c9e9a15f7d0eff542e45e18b42527bbb654aacbd
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-08-14 10:35:15 -0300

    Funcao addFriends-clean

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listFriends = [];
+
+function addFriends() {
+    let friend = document.getElementById('amigo').value;
+    
+    if(friend != '') {
+        listFriends.push(friend);
+
+        clean();
+    } else {
+        alert('Por favor, insira um nome.');
+    }
+}
+function clean() {
+    friend = document.querySelector('input');
+    friend.value = '';
+}
