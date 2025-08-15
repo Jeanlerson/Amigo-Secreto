934c2631138eb9b8e51a375745794a92678d768e
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-08-15 15:25:11 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listFriends = [];
 
+let ulFriend = document.getElementById('listaAmigos');
+let ulDrawFriends = document.getElementById('resultado');
+
 function addFriends() {
-    let friend = document.getElementById('amigo').value;
+    let friend = document.getElementById('amigo').value.trim();
     
+    if(ulDrawFriends.innerHTML != '') {
+        alert('Para adicionar de novo, deve fazer outro sorteio.');
+        return;
+    }
+
     if(friend != '') {
         listFriends.push(friend);
 
@@ -11,6 +19,7 @@ function addFriends() {
         showList();
     } else {
         alert('Por favor, insira um nome.');
+        clean();
     }
 }
 function clean() {
@@ -18,7 +27,6 @@ function clean() {
     friend.value = '';
 }
 function showList() {
-    let ulFriend = document.getElementById('listaAmigos');
     ulFriend.innerHTML = '';
 
     for(let friend = 0; friend < listFriends.length; friend++) {
@@ -29,7 +37,6 @@ function showList() {
 }
 function drawFriends() {
     let numberRandom = Math.floor(Math.random() * listFriends.length);
-    let ulDrawFriends = document.getElementById('resultado');
     ulDrawFriends.innerHTML = '';
 
     if(listFriends.length > 0) {
@@ -39,6 +46,11 @@ function drawFriends() {
 
         document.getElementById('listaAmigos').innerHTML = '';
     } else {
-        alert('Por favor, insire um nome antes.');
+        alert('Por favor, insira um nome antes.');
     }
+}
+function resetDraw() {
+    listFriends.length = 0;
+    ulDrawFriends.innerHTML = '';
+    ulFriend.innerHTML = '';
 }
